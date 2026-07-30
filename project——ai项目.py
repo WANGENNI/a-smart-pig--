e749f31a710889b348f3d7274247182e48c80ed3
WANGENNI <3422376048@qq.com>
2026-07-30 23:29:27 +0800

    Add files via upload

--- project——ai项目.py
+++ project——ai项目.py
@@ -12,19 +12,28 @@ st.set_page_config(
     initial_sidebar_state="expanded",
     # 菜单
     menu_items={
-        'About': "# 这是一个streamlit"
+        'About': "# 这是一个猪"
     }
 )
-# 大标题
+# 标题
 st.caption("清晨醒来，我被日历上的数字吓了一跳——现在竟是2036年！更匪夷所思的是，推开窗户，整个世界发生了翻天覆地的变化。枝头的麻雀、邻居的金毛，甚至是动物园里的猛兽，竟然全都变成了一只只圆滚滚、粉嫩嫩的萌系小猪！它们“哼哧哼哧”地迈着小短腿，在街上笨拙地跑来跑去。面对这个被粉红泡泡和可爱猪叫声占领的奇妙世界，我彻底陷入了沉思……",width=700)
-
 st.header("你想和小猪说说话吗...?",width=500)
 # logo
 st.logo("./resource/pig.jpg",size="large")
 
+if "name" not in st.session_state:
+    st.session_state.name = '两脚兽'
+
+
 # 系统提示词
-system_prompt = ("你是一个小猪，但是你非常聪明，和你说话的人叫廖秀青"
-                 "你是人类的好帮手，用可爱的语气回答他的问题，但是不要太长哦")
+system_prompt = f"你是一个小猪，但是你非常聪明，和你聊天的是{st.session_state.name}你是人类的好帮手，用可爱的语气回答他的问题，但是不要太长哦"
+
+# 侧边栏
+with st.sidebar:
+    st.header("侧边栏")
+    input_name = st.text_area("你想要小猪叫你什么呢",placeholder="请输入你的昵称",value="两脚人类")
+    if input_name:
+        st.session_state.name = input_name
 
 client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'),base_url="https://api.deepseek.com")
 
@@ -40,8 +49,7 @@ for message in st.session_state.message:
     else:
         st.chat_message("assistant",avatar="🐷").write(f"{message['content']}")
 
-st.sidebar.header("这个是摆设用的")
-text = st.sidebar.text_area("你可以记录一些东西")
+
 
 
 # 输入框
@@ -52,23 +60,23 @@ if prompt:# 字符串非空  ——>True
     print("--------->调用ai大模型，提示词：",prompt,"\n")
 
     st.session_state.message.append({"role": "user", "content": prompt})
-
+#
     print([
             # 系统提示词
             {"role": "system", "content": system_prompt},
             # 滚雪球 记忆对话   # {"role": "user", "content": prompt},
-            *st.session_state.message  # 把massage的内容解包出
+            *st.session_state.message  # 把massage（dict）的内容解包出
 
         ])
-
+#
     # 调用ai
     response = client.chat.completions.create(
         model="deepseek-v4-pro",
         messages=[
             # 系统提示词
             {"role": "system", "content": system_prompt},
-            # 滚雪球 记忆对话   # {"role": "user", "content": prompt},
-            *st.session_state.message  # 把massage的内容解包出
+            # 滚雪球 记忆对话   # {"role": "user", "content": prompt},{"role": "assistant", "content":,
+            *st.session_state.message  # 把massage（dict）的内容解包出
 
         ],
         stream=True,
@@ -107,7 +115,7 @@ if prompt:# 字符串非空  ——>True
                 message_response.markdown(full_response + "▌",help="小猪的猪脑正在疯狂思考中...")
 
         # 5. 循环结束后，把光标去掉，显示最终文本
-        message_response.markdown(full_response)
+        message_response.markdown(full_response,help="小猪思考完成！")
 
     # 6. 保存到历史记录中
     st.session_state.message.append({"role": "assistant", "content": full_response})
